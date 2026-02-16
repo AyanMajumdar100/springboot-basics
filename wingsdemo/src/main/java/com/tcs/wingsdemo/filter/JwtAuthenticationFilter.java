61ed99ffb73c4215e3393c120d94d35c085cc740
Ayan Majumdar <ayanmajumdar.x@gmail.com>
2026-02-17 01:21:21 +0530

    @NonNull annotation on its parameters to match the OncePerRequestFilter source.

--- wingsdemo/src/main/java/com/tcs/wingsdemo/filter/JwtAuthenticationFilter.java
+++ wingsdemo/src/main/java/com/tcs/wingsdemo/filter/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 
@@ -26,14 +27,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
 
         // 1. Get Token from Header
         String requestHeader = request.getHeader("Authorization");
         String username = null;
         String token = null;
 
-        if (requestHeader!= null && requestHeader.startsWith("Bearer ")) {
+        if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
             token = requestHeader.substring(7); // Remove "Bearer " prefix
             try {
                 username = jwtHelper.extractUsername(token);
@@ -43,10 +46,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         // 2. Validate Token
-        if (username!= null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
             if (jwtHelper.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
